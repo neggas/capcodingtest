7c13e83993453513fe8defba2e47f7b506a09e04
neggas <arsene.fop@gmail.com>
2024-09-30 15:32:02 +0000

    feat: add request rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { appEnv } from './utils/functions';
 import { APP_PIPE } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `env/.${appEnv()}.env`,
