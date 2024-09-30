a72cfda5678df08237bec822bb2690383b537269
neggas <arsene.fop@gmail.com>
2024-09-30 15:26:30 +0000

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Test-Api V1')
+    .setDescription('Test-Api V1 Api documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
